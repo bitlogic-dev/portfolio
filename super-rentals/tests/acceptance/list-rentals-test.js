e826c8ef6d6e46f9105e00604168fb1f4f3a4db1
Haig Hagopian <neofusionzero@users.noreply.github.com>
2019-11-25 12:23:23 -0800

    Add data to listings model and update acceptance test.

--- super-rentals/tests/acceptance/list-rentals-test.js
+++ super-rentals/tests/acceptance/list-rentals-test.js
@@ -35,7 +35,11 @@ module('Acceptance | list rentals', function(hooks) {
     assert.equal(currentURL(), '/contact', 'should navigate to contact');
   });
 
-  test('should list available rentals', async function(assert) { });
+  test('should list available rentals', async function(assert) {
+    await visit('/');
+    // querySelectorAll method returns the elements that match the given CSS selector.
+    assert.equal(this.element.querySelectorAll('.listing').length, 3, 'should display 3 listings');
+  });
 
   test('should filter the list of rentals by city', async function(assert) { });
 
