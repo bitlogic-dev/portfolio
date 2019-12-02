febc1f095b1e22f775fffce36e218b3b7f2c49dc
Haig Hagopian <neofusionzero@users.noreply.github.com>
2019-12-02 15:53:15 -0800

    Update configurations.

--- config/environment.js
+++ config/environment.js
@@ -52,6 +52,7 @@ module.exports = function(environment) {
   if (environment === 'production') {
     // here you can enable a production-specific feature
     ENV.modulePrefix = 'super-rentals-app';
+    ENV.rootURL = '/super-rentals-app/';
   }
 
   return ENV;
