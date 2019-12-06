c9f76013c2d799f17cd0c814b1987a6408c02d51
Haig Hagopian <haig.hagopian@bydeluxe.com>
2019-12-05 17:07:39 -0800

    Added projects page and loaded with a single sample project-item component.

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,8 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  // use a route lifecycle hook called redirect to load /rentals when a user visits /
+  // use a route lifecycle hook called redirect to load /projects when a user visits /
   redirect() {
-    this.transitionTo('rentals');
+    this.transitionTo('projects');
   }
 });
