d7762e7046e717f82357b01160dcb5a5717b2af1
Haig Hagopian <neofusionzero@users.noreply.github.com>
2019-12-23 18:22:37 -0800

    Added integration test for project-item component.

--- tests/integration/components/project-item-test.js
+++ tests/integration/components/project-item-test.js
@@ -2,25 +2,24 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import EmberObject from '@ember/object';
 
 module('Integration | Component | project-item', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+  // create a test object with the expected properties of a rental
+  hooks.beforeEach(function() {
+    this.project = EmberObject.create({
+      title: 'test-title',
+      category: 'test-category',
+      image: 'test-image',
+      url: 'test-url',
+      description: 'test-description'
+    });
+  });
 
-    await render(hbs`<ProjectItem />`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      <ProjectItem>
-        template block text
-      </ProjectItem>
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+  test('should display project details', async function(assert) {
+    await render(hbs`<ProjectItem @project={{this.project}} />`);
+    assert.equal(this.element.querySelector('.listing h3').textContent.trim(), 'test-title', 'test-title');
   });
 });
